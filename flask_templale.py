7bce40daf93fef7b5771d34df74d1822fe5db21a
mgreen01 <morgangreen0681@outlook.com>
2021-10-20 20:21:20 +0100

    HTML Template Generation

---
+++ flask_templale.py
@@ -0,0 +1,10 @@
+from logging import debug
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<html><body><h1>Hello World</h1></body></html>'
+
+if __name__ == "__main__":
+    app.run(debug = True)
